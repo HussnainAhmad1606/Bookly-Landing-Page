5c5d35524e37fa3725837598870c987a9e3dbe59
PsychoZexta <zohaib.saeed1259@gmail.com>
2023-08-01 20:16:48 +0500

    Created Navbar and hero section

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 // App.js
+const toggleBtn = document.querySelector('.toggle-btn');
+        const toggleBtnIcon = document.querySelector('.toggle-btn i');
+        const dropdownMenu = document.querySelector('.dropdown-menu');
+
+        toggleBtn.onclick = function () {
+            dropdownMenu.classList.toggle('open')
+        }
