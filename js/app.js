4c322a01b2ff72c8af3eb1d39399791c9d05158d
Hussnain Ahmad <75199596+HussnainAhmad1606@users.noreply.github.com>
2024-05-15 23:18:36 +0500

    change music

--- js/app.js
+++ js/app.js
@@ -7,3 +7,13 @@ const dropdownMenu = document.querySelector('.dropdown-menu');
 toggleBtn.onclick = function () {
     dropdownMenu.classList.toggle('open')
 }
+
+
+var audio = document.createElement("AUDIO")
+document.body.appendChild(audio);
+audio.src = "../music/test.mp3"
+
+document.body.addEventListener("mousemove", function () {
+    console.log("Playing")
+    audio.play()
+})
