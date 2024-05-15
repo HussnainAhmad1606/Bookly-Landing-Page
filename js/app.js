be68df4f495658279e50355bc44ebc208d8b8aa4
Hussnain Ahmad <75199596+HussnainAhmad1606@users.noreply.github.com>
2024-05-15 23:43:38 +0500

    add music

--- js/app.js
+++ js/app.js
@@ -7,13 +7,35 @@ const dropdownMenu = document.querySelector('.dropdown-menu');
 toggleBtn.onclick = function () {
     dropdownMenu.classList.toggle('open')
 }
+document.addEventListener("DOMContentLoaded", function() {
+    // Create an audio element
+    var audio = new Audio("../music/test.mp3");
+
+    // Autoplay the audio when it's loaded
+    audio.autoplay = true;
+
+    audio.controls = true;
+    
+    // Append the audio element to the document body
+    document.body.appendChild(audio);
+
+    var test = audio.play();
+
+    
+if (test !== undefined) {
+    test.then(_ => {
+        alert("Started")
+        // Autoplay started!
+    }).catch(error => {
+        alert("Autoplay Disabled")
+      // Autoplay was prevented.
+      // Show a "Play" button so that user can start playback.
+    });
+  }
 
 
-var audio = document.createElement("AUDIO")
-document.body.appendChild(audio);
-audio.src = "../music/test.mp3"
-
-document.body.addEventListener("mousemove", function () {
-    console.log("Playing")
-    audio.play()
-})
+    // Optionally, you can pause the audio after a certain time (e.g., 10 seconds)
+    setTimeout(function() {
+        audio.pause();
+    }, 10000); // Pause after 10 seconds (10000 milliseconds)
+});
